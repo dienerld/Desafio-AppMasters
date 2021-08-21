66d69ed168ba818f0bdeacddac9935111d30ee51
Diener <dienezim@hotmail.com>
2021-08-21 18:19:13 -0300

    create router  post

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import router from './routes';
 
 const app = express();
 const port = 5500;
-
+app.use(express.json());
 app.use('/', router);
 
 app.listen(port, () => console.log(`App listening on port http://localhost:${port}`));
--- src/routes.ts
+++ src/routes.ts
@@ -3,10 +3,19 @@ import express from 'express';
 
 const router = express.Router();
 
+interface jsonSteam {
+  appid: number;
+  name: string;
+}
+
+let fullData: jsonSteam[];
+let gameDetails;
+
 router.get('/', async (req, res) => {
   try {
     const { data } = await axios('https://api.steampowered.com/ISteamApps/GetAppList/v0002/?format=json');
-    return res.send(data.applist.apps);
+    fullData = data.applist.apps;
+    return res.send(fullData);
   } catch (error) {
     console.log(error);
     return null;
@@ -17,12 +26,18 @@ router.get('/:id', async (req, res) => {
     const appId = req.params.id;
     console.log(appId);
     const { data } = await axios(`https://store.steampowered.com/api/appdetails?appids=${appId}`);
-
-    return res.send(data[appId].data);
+    gameDetails = data[appId].data;
+    return res.send(gameDetails);
   } catch (error) {
     console.log(error);
     return res.status(404).send('Error');
   }
+});
+
+router.post('/favorite/:id', async (req, res) => {
+  console.log(req.body);
+
+  return res.send();
 });
 
 export default router;
